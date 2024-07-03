007aecdd4eba8763a315acfa99fa0e935ac86b1d
nexusameer <ameerk10fw@gmail.com>
2024-07-03 11:04:18 +0500

    3rd Problem

--- longest_substing_without_repeating.py
+++ longest_substing_without_repeating.py
@@ -1,1 +1,18 @@
-# hy boss
+def lengthOfLongestSubstring(s):
+    n = len(s)
+    max_lentgh = 0
+    left = 0
+    seen_chars = set()
+
+    for right in range(n):
+        while s[right] in seen_chars:
+            seen_chars.remove(s[left])
+            left += 1
+        seen_chars.add(s[right])
+        max_lentgh = max(max_lentgh, right-left+1)
+    return max_lentgh        
+        
+
+
+s = 'abdaaabdsaaae'
+print(lengthOfLongestSubstring(s))
